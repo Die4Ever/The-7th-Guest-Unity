7cc983d6355ff5cddcf451cbfcea13169e7d08bb
Die4Ever <30947252+Die4Ever@users.noreply.github.com>
2018-03-12 21:04:50 -0500

    Update extract.bat

--- extract.bat
+++ extract.bat
@@ -4,6 +4,6 @@ for /f "tokens=*" %%f in ('dir /b ..\\*.gjd 2^>nul') do (
   SET rlfile=%%f
   SET rlfile=!rlfile:.gjd=.rl!
   echo "extracting %%f and !rlfile!"
-  T7GGrvEx\T7GGrvEx.exe "..\!rlfile!" "..\%%f"
+  t7g-toolkit\T7GGrvEx\T7GGrvEx.exe "..\!rlfile!" "..\%%f"
 )
 pause
