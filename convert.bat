f4284a4159a562e59006d5a7bfcb739b74d72ee1
Die4Ever <30947252+Die4Ever@users.noreply.github.com>
2018-03-12 21:07:48 -0500

    Update convert.bat

--- convert.bat
+++ convert.bat
@@ -1,7 +1,4 @@
 @echo off
 SETLOCAL ENABLEDELAYEDEXPANSION
-for /f "tokens=*" %%f in ('dir /b data\\*.vdx 2^>nul') do (
-  echo "converting %%f"
-  t7g-toolkit\VDXExt\VDXExt.exe /avi /sync "data\%%f"
-)
+t7g-toolkit\VDXExt\VDXExt.exe /avi /sync "data\*.vdx"
 pause
