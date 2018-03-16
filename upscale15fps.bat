48adc59aa5096248d6ee37568ed3823f6c4a8ac4
Ray Carro <die4ever2005@gmail.com>
2018-03-16 16:16:21 -0500

    added upscale15fps.bat for upscaling without motion interpolation

---
+++ upscale15fps.bat
@@ -0,0 +1,8 @@
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+mkdir upscaled
+for /f "tokens=*" %%f in ('dir /b *.avi') do (
+  echo "upscaling %%fupscaled\\%%f"
+  start /B /low ffmpeg -i "%%f" -b:a 256k -b:v 20M -filter_complex "[0:v]deband='r=16:1thr=0.02:2thr=0.02:3thr=0.02:4thr=0.02', xbr=3[v];[0:a]acopy[a]" -map "[v]" -map "[a]" "upscaled\\%%f"
+)
+pause
