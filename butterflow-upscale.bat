5d69d3a5f552046a5865b994d078dcdec45a8636
Ray Carro <die4ever2005@gmail.com>
2018-03-16 16:17:23 -0500

    added sharpen filter

--- butterflow-upscale.bat
+++ butterflow-upscale.bat
@@ -5,7 +5,7 @@ mkdir temp
 for /f "tokens=*" %%f in ('dir /b *.avi') do (
   echo "upscaling %%fupscaled\\%%f"
   "..\..\Downloads\butterflow-0.2.3\butterflow.exe" -audio -l --poly-s=0.1 -r60 "%%f" -o "temp\\%%f-butterflow.mp4"
-  ffmpeg -i "temp\\%%f-butterflow.mp4" -b:a 256k -b:v 20M -filter_complex "[0:v]xbr=4[v];[0:a]acopy[a]" -map "[v]" -map "[a]" "upscaled\\butterflow-%%f"
+  ffmpeg -i "temp\\%%f-butterflow.mp4" -b:a 256k -b:v 20M -filter_complex "[0:v]xbr=4, unsharp[v];[0:a]acopy[a]" -map "[v]" -map "[a]" "upscaled\\butterflow-%%f"
   del "temp\\%%f-butterflow.mp4"
 )
 pause
