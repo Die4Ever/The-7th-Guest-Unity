a0d18b9bd57ee9c5c753d78ba4eef772ef33bb47
Ray Carro <die4ever2005@gmail.com>
2018-03-22 20:17:30 -0500

    slightly reduced upscaling to target 1080p and spend less time converting videos

--- upscale.bat
+++ upscale.bat
@@ -3,6 +3,6 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 mkdir upscaled
 for /f "tokens=*" %%f in ('dir /b *.avi') do (
   echo "upscaling %%fupscaled\\%%f"
-  start /B /low ffmpeg -i "%%f" -b:a 256k -b:v 20M -filter_complex "[0:v]deband='r=16:1thr=0.02:2thr=0.02:3thr=0.02:4thr=0.02', minterpolate='fps=60:mi_mode=mci:scd=none:vsbmc=1', xbr=4, unsharp[v];[0:a]acopy[a]" -map "[v]" -map "[a]" "upscaled\\%%f"
+  start /B /low ffmpeg -i "%%f" -b:a 256k -b:v 20M -filter_complex "[0:v]deband='r=16:1thr=0.02:2thr=0.02:3thr=0.02:4thr=0.02', minterpolate='fps=60:mi_mode=mci:scd=none:vsbmc=1', xbr=3, unsharp[v];[0:a]acopy[a]" -map "[v]" -map "[a]" "upscaled\\%%f"
 )
 pause
